78dc8278e1ca52920ed1309a6f29ce0acdda9802
Siva Kumar Bala Krishnan <siva-kumar.bala-krishnan@dxc.com>
2020-03-16 22:08:42 +0530

    end of section 8

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,5 +9,7 @@ namespace DatingApp.API.Data
         public DataContext([NotNullAttribute] DbContextOptions options) : base(options){}
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
     }
 }
